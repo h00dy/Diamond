ffac0cfcddc68c519bcde57daa3b5bd1f4732b70
Maciej Jagiellowicz <maciej.jagiellowicz@allegrogroup.com>
2015-07-31 12:10:01 +0200

    kairosDB handler and collectors tag support

---
+++ src/diamond/handler/kairosdb.py
@@ -0,0 +1,147 @@
+"""
+Send metrics to a [KairosDB](http://kairosdb.github.io/) using telnet or rest
+method.
+
+"""
+
+from Handler import Handler
+import socket
+
+
+class KairosDBHandler(Handler):
+    """
+    Implements the abstract Handler class, sending data to kairosdb
+    """
+
+    RETRY = 3
+
+    def __init__(self, config=None):
+        """
+        Create a new instance of the KairosDBHandler class
+        """
+        # Initialize Handler
+        Handler.__init__(self, config)
+
+        # Initialize Data
+        self.socket = None
+
+        # Initialize Options
+        self.host = self.config['host']
+        self.port = int(self.config['port'])
+        self.timeout = int(self.config['timeout'])
+
+        # Connect
+        self._connect()
+
+    def get_default_config_help(self):
+        """
+        Returns the help text for the configuration options for this handler
+        """
+        config = super(KairosDBHandler, self).get_default_config_help()
+
+        config.update({
+            'host': 'Hostname',
+            'port': 'Port',
+            'timeout': '',
+        })
+
+        return config
+
+    def get_default_config(self):
+        """
+        Return the default config for the handler
+        """
+        config = super(KairosDBHandler, self).get_default_config()
+
+        config.update({
+            'host': 'localhost',
+            'port': 4242,
+            'timeout': 15,
+        })
+
+        return config
+
+    def __del__(self):
+        """
+        Destroy instance of the KairosDBHandler class
+        """
+        self._close()
+
+    def process(self, metric):
+        """
+        Process a metric by sending it to KairosDB
+        """
+        # Append the data to the array as a string
+        command = "put {metric} {timestamp} {value} {tags} \n".format(
+            metric="{}.{}".format(metric.getCollectorPath(),
+                                  metric.getMetricPath()),
+            timestamp=metric.timestamp,
+            value=metric.value,
+            tags=metric.tags or ''
+        )
+        self._send(command)
+
+    def _send(self, data):
+        """
+        Send data to KairosDB. Data that can not be sent will be queued.
+        """
+        retry = self.RETRY
+        # Attempt to send any data in the queue
+        while retry > 0:
+            # Check socket
+            if not self.socket:
+                # Log Error
+                self.log.error("KairosDBHandler: Socket unavailable.")
+                # Attempt to restablish connection
+                self._connect()
+                # Decrement retry
+                retry -= 1
+                # Try again
+                continue
+            try:
+                # Send data to socket
+                self.socket.sendall(data)
+                self.log.debug("RUNNING: {}".format(data))
+                # Done
+                break
+            except socket.error, e:
+                self.log.error("KairosDBHandler: Failed sending data. %s.", e)
+                # Attempt to restablish connection
+                self._close()
+                # Decrement retry
+                retry -= 1
+                # try again
+                continue
+
+    def _connect(self):
+        """
+        Connect to the KairosDB server
+        """
+        # Create socket
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if socket is None:
+            self.log.error("KairosDBHandler: Unable to create socket.")
+            # Close Socket
+            self._close()
+            return
+        # Set socket timeout
+        self.socket.settimeout(self.timeout)
+        # Connect to KairosDB server
+        try:
+            self.socket.connect((self.host, self.port))
+            self.log.debug("Established connection to KairosDB server %s:%d",
+                           self.host, self.port)
+        except Exception, ex:
+            self.log.error("KairosDBHandler: Failed to connect to %s:%i. %s",
+                           self.host, self.port, ex)
+            # Close Socket
+            self._close()
+            return
+
+    def _close(self):
+        """
+        Close the socket
+        """
+        if self.socket is not None:
+            self.socket.close()
+        self.socket = None
