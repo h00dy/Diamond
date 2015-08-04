3c99930c26684d332e8d2533524f0d9ebfc6e963
Hubert Sajnaj <hubert.sajnaj@allegro.pl>
2015-08-04 12:16:42 +0200

    Changed Tag declaration in conf and added default tag [host]

--- src/diamond/handler/kairosdb.py
+++ src/diamond/handler/kairosdb.py
@@ -2,22 +2,39 @@
 Send metrics to a [KairosDB](http://kairosdb.github.io/) using telnet or rest
 method.
 
+add config to diamond.conf:
+
+[[KairosDBHandler]]
+host = localhost
+port = 4242
+
+Optionaly if you like to usa Tags (Metrics 2.0), you should add field [[[tags]]] in collector config.
+Example:
+
+[[CPUCollector]]
+enabled = True
+[[[tags]]]
+env=develop
+host=localhost
+
 """
 
 from Handler import Handler
+from diamond.collector import get_hostname
 import socket
 
 
 class KairosDBHandler(Handler):
     """
-    Implements the abstract Handler class, sending data to kairosdb
+    Implements the abstract Handler class, sending data to kairosdb.
+    It sets by default tag [host] which indicates hostname.
     """
 
     RETRY = 3
 
     def __init__(self, config=None):
         """
-        Create a new instance of the KairosDBHandler class
+        Create a new instance of the KairosDBHandler class.
         """
         # Initialize Handler
         Handler.__init__(self, config)
@@ -29,13 +46,13 @@ class KairosDBHandler(Handler):
         self.host = self.config['host']
         self.port = int(self.config['port'])
         self.timeout = int(self.config['timeout'])
-
+        self.hostname = get_hostname(self.config)
         # Connect
         self._connect()
 
     def get_default_config_help(self):
         """
-        Returns the help text for the configuration options for this handler
+        Returns the help text for the configuration options for this handler.
         """
         config = super(KairosDBHandler, self).get_default_config_help()
 
@@ -49,7 +66,7 @@ class KairosDBHandler(Handler):
 
     def get_default_config(self):
         """
-        Return the default config for the handler
+        Return the default config for the handler.
         """
         config = super(KairosDBHandler, self).get_default_config()
 
@@ -58,26 +75,48 @@ class KairosDBHandler(Handler):
             'port': 4242,
             'timeout': 15,
         })
-
         return config
 
     def __del__(self):
         """
-        Destroy instance of the KairosDBHandler class
+        Destroy instance of the KairosDBHandler class.
         """
         self._close()
 
+    @staticmethod
+    def _tags_parser(tags):
+        """
+        Parse tags to KairosDB format "key1=value1 key2=value2...keyN=valueN".
+        """
+        parsed_tags = ''
+        if isinstance(tags, dict):
+            for key, value in tags.iteritems():
+                parsed_tags += "{key}={value} ".format(key=key, value=value)
+
+        else:
+            parsed_tags = tags
+        return parsed_tags
+
     def process(self, metric):
         """
-        Process a metric by sending it to KairosDB
+        Process a metric by sending it to KairosDB.
         """
         # Append the data to the array as a string
+        # Add default tag
+
+        host = "host={hostname}".format(hostname=self.hostname)
+
+        tags = self._tags_parser(metric.tags or '')
+
+        if not 'host' in tags:
+            tags += host
+
         command = "put {metric} {timestamp} {value} {tags} \n".format(
             metric="{}.{}".format(metric.getCollectorPath(),
                                   metric.getMetricPath()),
             timestamp=metric.timestamp,
             value=metric.value,
-            tags=metric.tags or ''
+            tags=tags
         )
         self._send(command)
 
@@ -115,7 +154,7 @@ class KairosDBHandler(Handler):
 
     def _connect(self):
         """
-        Connect to the KairosDB server
+        Connect to the KairosDB server.
         """
         # Create socket
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -140,7 +179,7 @@ class KairosDBHandler(Handler):
 
     def _close(self):
         """
-        Close the socket
+        Close the socket.
         """
         if self.socket is not None:
             self.socket.close()
